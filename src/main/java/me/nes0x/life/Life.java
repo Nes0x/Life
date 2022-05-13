2ef84dcd6ebf1ce1205e4c901bd3e6b26c25bbd4
Nes0x <przemek882@onet.pl>
2022-05-13 14:32:16 +0200

    changed pl-config save way

--- src/main/java/me/nes0x/life/Life.java
+++ src/main/java/me/nes0x/life/Life.java
@@ -33,33 +33,11 @@ public final class Life extends JavaPlugin {
         getConfig().options().copyDefaults(true);
         saveConfig();
         reloadConfig();
+        saveResource("pl-config.yml", true);
         File usersFolder = new File(getDataFolder(), "./users");
         if (!usersFolder.exists()) {
             usersFolder.mkdir();
         }
-
-        OutputStream outStream = null;
-        try {
-            ByteArrayOutputStream os = new ByteArrayOutputStream();
-            byte[] buffer = new byte[0xFFFF];
-            InputStream inputStream =  getResource("pl-config.yml");
-            for (int len = inputStream.read(buffer); len != -1; len = inputStream.read(buffer)) {
-                os.write(buffer, 0, len);
-            }
-            File targetFile = new File(getDataFolder(), "./pl-config.yml");
-            outStream = Files.newOutputStream(targetFile.toPath());
-            outStream.write(os.toByteArray());
-        } catch (IOException exception) {
-            exception.printStackTrace();
-        } finally {
-            try {
-                if (outStream != null) outStream.close();
-            } catch (IOException exception) {
-                Bukkit.getLogger().info(fixColors("&cFatal error occurred!"));
-                getPluginLoader().disablePlugin(this);
-            }
-        }
-
 
         if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
             new Placeholder(this).register();
